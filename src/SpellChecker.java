ecc706fc19d74b95b7df4e4c567dd79dbae43ec4
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-06-13 22:31:57 +0300

    small fixes

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -7,7 +7,8 @@ public class SpellChecker {
     {
         //get text files
         FileInputStream dictFile = FileHelper.readTextFile("src\\DictionaryFile.txt"); //read dictionary file
-        FileInputStream inputFile = FileHelper.readTextFile("src\\InputFile.txt"); // read input file
+//        FileInputStream inputFile = FileHelper.readTextFile("src\\InputFile.txt"); // read input file
+        FileInputStream inputFile = FileHelper.readTextFile("src\\Test.txt"); // read input file
         //insert dictionary to hash table
         HashMap<String,Integer> dictMap = new HashMap<String, Integer>();// initialize hash table
         FileHelper.insertFileToMap(dictFile,dictMap); // insert dictionary words to hash table
@@ -17,7 +18,13 @@ public class SpellChecker {
         // delete words in dictionary from the input tree
         deleteCorrectWords(inputTree,dictMap);
         //print list of suspicious words
-        printTree(inputTree);
+        printIncorrectWords(inputTree);
+
+//        System.out.println("'him' in tree: "+inputTree.search("him").notLeaf());
+//        System.out.println("'the' in map: "+dictMap.containsKey("him"));
+//        inputTree.delete("him");
+//        System.out.println("'him' in tree: "+inputTree.search("him").notLeaf());
+
     }
     private static void deleteCorrectWords(RedBlackTree<String> tree, HashMap<String,Integer> dictMap)
     {
@@ -30,20 +37,28 @@ public class SpellChecker {
             if(dictMap.containsKey(p.key)) //current not contain word in the dictionary
             {
                 tree.delete(p); // delete correct word from tree
+//                tree.delete("him");
+//                System.out.println("him deleted");
             }
             deleteCorrectWords(p.getLeft(),tree,dictMap);//left sub tree
             deleteCorrectWords(p.getRight(),tree,dictMap);// right sub tree
         }
     }
-    private static void printTree(RedBlackTree<String> tree)
+    private static void printIncorrectWords(RedBlackTree<String> tree)
     {
         ArrayList<String> incorrectWords = tree.getListInOrder();
-        System.out.println();
-        System.out.println("Suspicious words:");
-        for (int i = 0; i <incorrectWords.size()-1 ; i++) {
-            System.out.print(incorrectWords.get(i)+", ");
+        if(incorrectWords.size()>0) {
+            System.out.println("Suspicious words:");
+            System.out.println();
+            for (int i = 0; i < incorrectWords.size() - 1; i++) {
+                System.out.print(incorrectWords.get(i) + ", ");
+            }
+            System.out.print(incorrectWords.get(incorrectWords.size() - 1));
         }
-        System.out.print(incorrectWords.get(incorrectWords.size()-1));
+        else
+        {
+            System.out.println("Spelling seems ok");
+        }
         System.out.println();
 
     }
